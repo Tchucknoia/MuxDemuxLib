55418b312776b7cd26f86e79f7f8bd8ad11440f7
Tiago André Lachman <58074774+Tchucknoia@users.noreply.github.com>
2021-10-29 15:47:00 -0300

    Update demuxClass.h

--- DemuxLib/demuxClass.h
+++ DemuxLib/demuxClass.h
@@ -10,9 +10,10 @@
 #define MuxClass.h
 
 //Erros do demux
-#define MUX_OK        0
-#define UNKNOWN         1
-#define NOT_IN_RANGE    2
+#define MUX_OK          520
+#define UNKNOWN         521
+#define NOT_IN_RANGE    522
+#define WRONG_MODE      523
 
 
 //Tipos de controle disponíveis do Demux
@@ -22,20 +23,31 @@
 
 class Mux{
 private:
-    int* inhEnables;//Pinos dos inhEnables
-    int* controls;//Pinos de comando do mux
+    int mode;
+    int* common;//Pino comum do mux
+    int* inhEnables;//Pinos dos inhEnables do mux/demux
+    int* controls;//Pinos de comando do mux/demux
     int numInhEnables;//Tamanho do vetor inhEnables
     int numControls;//Tamanho do vetor controls
-    int numSaidas;//Números de saídas/entradas disponíveis para controle
+    int numSaidas;//Números de saídas/entradas disponíveis para controle do mux/demux
     int potControls;//Variavel para armazenar quantas combinações são 
-                    //possiveis dado o numero de pinos de controle
+                    //possiveis dado o numero de pinos de controle do mux/demux
 
 public:
     Mux(
         int* pinInhEnables,
         int sizeInhEnables,
         int* pinControls,
-        int sizeControls
+        int sizeControls,
+        int mode
+    );
+    Mux(
+        int* pinInhEnables,
+        int sizeInhEnables,
+        int* pinControls,
+        int sizeControls,
+        int* pinCom,
+        int mode
     );
 
     int ligarSaida(int numOut);//Liga uma saída do multiplexador
@@ -43,6 +55,8 @@ public:
 
 
     int maxSaidas();//Retorna o número de saídas disponíveis
+
+    int showMode();//Retorna o modo definido
 
 protected:
     void paraBin(int num, bool* vetor, int tam=3){ //Decimal para binário
